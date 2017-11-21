8c6c9b73db16d786627ebfdd1a4db3fa2cbb3500
Tom Vidar Michalsen <tomvidm@gmail.com>
2017-11-22 00:21:53 +0100

    Added a few names.

---
+++ src/scripts/names.lua
@@ -0,0 +1,242 @@
+names = {
+    arabic = {
+        first_names = {
+            male = {
+                "Muammar",
+                "Mohammad",
+                "Saddam",
+                "Bashir"
+            },
+
+            female = {
+                "Manar",
+                "Abir",
+                "Alya",
+                "Shabnam"
+            }
+        },
+
+        last_names = {
+            "Al-Assad",
+            "Hussein",
+            "Al-Anwar",
+            "Al-Something",
+            "Muahmmad"
+        }
+    },
+
+    english = {
+        first_names = {
+            male = {
+                "Arthur",
+                "Bruce",
+                "Charles",
+                "David",
+                "Edgar",
+                "Frank",
+                "Greg",
+                "Hugh",
+                "Irwin",
+                "James",
+                "Matthew",
+                "Neil",
+                "Patrick"
+            },
+
+            female = {
+                "Alice",
+                "Anne"
+            }
+
+        last_names = {
+            "Baker",
+            "Smith",
+            "Bush",
+            "Clinton",
+            "Kerrington",
+            "Potter",
+            "Lagerfeld",
+            "Nguyen"
+        }
+    },
+
+    scandinavian = {
+        first_names = {
+            male = {
+                "Andre",
+                "Arne",
+                "Atle",
+                "Anton",
+                "Andreas",
+                "Anders",
+                "Bjorn",
+                "Christian",
+                "Christer",
+                "David",
+                "Daniel",
+                "Elias",
+                "Erik",
+                "Eirik",
+                "Endre",
+                "Emil",
+                "Frank",
+                "Filip",
+                "Georg",
+                "Harald",
+                "Henning",
+                "Johan",
+                "John",
+                "Jan",
+                "Kristian",
+                "Krister",
+                "Kurt",
+                "Lennart",
+                "Ragnar"
+            },
+
+            female = {
+                "Anna",
+                "Anne",
+                "Beate",
+                "Birgit",
+                "Cecilie",
+                "Dina",
+                "Elise",
+                "Elisabeth",
+                "Else",
+                "Froya",
+                "Hilde",
+                "Helene",
+                "Helena",
+                "Herborg",
+                "Jorunn",
+                "Johanne",
+                "Johanna",
+                "Marit",
+                "Marte",
+                "Marta",
+                "Marie",
+                "Maria",
+                "Marianne",
+                "Pernille",
+                "Ragnhild",
+                "Ragna",
+                "Silje",
+                "Susanne",
+                "Sigrid",
+                "Sanne",
+                "Tove",
+                "Tuva",
+                "Torunn",
+                "Unni",
+                "Vilde",
+                "Veronika",
+                "Aase"
+            }
+        }
+    }
+
+    -- Polish, Czech, Slovenian etc
+    west_slavic = {
+        first_names = {
+            male = {
+                "Amadeusz",
+                "Arkadiusz",
+                "Boleslaw",
+                "Cezary",
+                "Cyprian",
+                "Mateusz",
+                "Michal",
+                "Karol",
+                "Piotr",
+                "Szymon"
+            },
+
+            female = {
+                "Beata",
+                "Malgorzata",
+                "Marta"
+            }
+        },
+
+        last_names = {
+            "Blysz",
+            "Kryze"
+        }
+    },
+
+    east_slavic = {
+        first_names = {
+            male = {
+                "Alexandr",
+                "Andrey",
+                "Anton",
+                "Anatoly",
+                "Artyom",
+                "Bohdan",
+                "Dmitry",
+                "Denys",
+                "Evgeny",
+                "Valeriy",
+                "Vasily",
+                "Nikita",
+                "Nikolay",
+                "Mikhail",
+                "Maksim",
+                "Kiril",
+                "Piotr",
+                "Sergey",
+                "Vyacheslav",
+                "Vladyslav",
+                "Yaroslav",
+                "Vitaly",
+                "Igor",
+                "Vladimir",
+                "Ivan",
+                "Taras",
+                "Tymofey",
+                "Fyodr"
+            },
+
+            female = {
+                "Anna",
+                "Alevtyna",
+                "Bohdana",
+                "Evgeniya",
+                "Elena",
+                "Elisaveta",
+                "Valeriya",
+                "Valentina",
+                "Ekaterina",
+                "Krystyna",
+                "Julia",
+                "Nadezhda",
+                "Svetlana",
+                "Ivanna",
+                "Olga",
+                "Tatyana",
+                "Natalya",
+                "Varvara",
+                "Alysa",
+                "Anastaniya",
+                "Oksana",
+                "Yaroslava",
+                "Diana",
+                "Alyona",
+                "Ludmila"
+            }
+        },
+
+        last_names = {
+            "Poroshenko",
+            "Kyrychenko",
+            "Yakovenko",
+            "Ishchenko",
+            "Tkachenko",
+            "Tymoshenko",
+            "Kondratenko",
+            "Komchatnykh",
+            "Levitskikh"
+        }
+    }
+}
